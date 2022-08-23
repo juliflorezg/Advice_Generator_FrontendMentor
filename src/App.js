94b5b8d0d3e92b6d91de022e2e034baf06ff511f
Julian Florez <forezjuli08@hotmail.com>
2022-08-23 18:38:08 -0500

    Modifying App, deleting console logs

--- src/App.js
+++ src/App.js
@@ -14,14 +14,9 @@ function App() {
       const response = await fetch('https://api.adviceslip.com/advice')
       const data = await response.json()
 
-      console.log(data)
-      console.log(data.slip.id)
-      console.log(data.slip.advice)
-
       setAdviceId(data.slip.id)
       setAdviceText(data.slip.advice)
       setIsLoading(false)
-      // setMakePetition(false)
     }
 
     getAdvice()
